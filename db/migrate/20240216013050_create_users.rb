b6efb2f96da5cc9b9c5a4487616c9489d6adb5da
Josh Pigford <josh@joshpigford.com>
2024-02-15 19:31:54 -0600

    Init + User model

---
+++ db/migrate/20240216013050_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :name
+      t.text :bio
+      t.string :location
+      t.datetime :joined_at
+
+      t.timestamps
+    end
+  end
+end
